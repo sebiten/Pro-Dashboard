913451bfb3ab3600a31224ea40b7b278d745a554
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-09-07 13:10:48 -0300

    Responsive dashboard finished

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable mini drone',
+        productNumber: '234555',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'Tablet',
+        productNumber: '234665',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'Phone',
+        productNumber: '234655',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+
+    },
+    {
+        productName: 'Smart tv',
+        productNumber: '214555',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'Bed',
+        productNumber: '234255',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+
+    },
+    {
+        productName: 'Table',
+        productNumber: '2355',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+]
